61779b92ee8aa0d05dbc3ba718957df68344b9f1
magisterblaman <hannes.blaman@lbs.se>
2024-10-17 12:17:15 +0200

    Binärsökning

--- AW23_PRRPRR01_10_Sökalgoritmer/Program.cs
+++ AW23_PRRPRR01_10_Sökalgoritmer/Program.cs
@@ -14,11 +14,29 @@ namespace AW23_PRRPRR01_10_Sökalgoritmer {
 			}
 			return -1;
 		}
-		static void Main(string[] args) {
-			int[] ints = new int[] { 46, 127, 121, 7, 88, 58, 124, 19, 79 };
-			int foundIndex = LinearSearch(ints, 40);
+		static bool BinarySearch(int[] numbers, int target) {
+			int left = 0;
+			int right = numbers.Length - 1;
+			while (left <= right) {
+				int checkIndex = (left + right) / 2;
+				int checkValue = numbers[checkIndex];
 
-			Console.WriteLine(foundIndex);
+				if (checkValue == target) {
+					return true;
+				} else if (checkValue > target) {
+					right = checkIndex - 1;
+				} else {
+					left = checkIndex + 1;
+				}
+			}
+
+			return false;
+		}
+		static void Main(string[] args) {
+			int[] ints = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25 };
+			bool isFound = BinarySearch(ints, 10);
+
+			Console.WriteLine(isFound);
 		}
 	}
 }
