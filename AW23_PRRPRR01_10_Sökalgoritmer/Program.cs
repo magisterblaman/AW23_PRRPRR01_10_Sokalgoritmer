e48f9c6528dc25e9206878dbeebd498c487b45ac
magisterblaman <hannes.blaman@lbs.se>
2024-10-17 11:39:31 +0200

    Linjärsökning

--- AW23_PRRPRR01_10_Sökalgoritmer/Program.cs
+++ AW23_PRRPRR01_10_Sökalgoritmer/Program.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace AW23_PRRPRR01_10_Sökalgoritmer {
 	internal class Program {
+		static int LinearSearch(int[] numbers, int target) {
+			for (int i = 0; i < numbers.Length; i++) {
+				if (numbers[i] == target) {
+					return i;
+				}
+			}
+			return -1;
+		}
 		static void Main(string[] args) {
+			int[] ints = new int[] { 46, 127, 121, 7, 88, 58, 124, 19, 79 };
+			int foundIndex = LinearSearch(ints, 40);
+
+			Console.WriteLine(foundIndex);
 		}
 	}
 }
