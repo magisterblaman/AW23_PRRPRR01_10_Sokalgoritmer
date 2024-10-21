faec423e1f09248eb1df332266676d32fc776605
magisterblaman <hannes.blaman@lbs.se>
2024-10-21 14:04:10 +0200

    Google teknisk intervjufråga

--- AW23_PRRPRR01_10_Sökalgoritmer/Program.cs
+++ AW23_PRRPRR01_10_Sökalgoritmer/Program.cs
@@ -32,9 +32,25 @@ namespace AW23_PRRPRR01_10_Sökalgoritmer {
 
 			return false;
 		}
+
+		static bool HasPairWithSum(int[] data, int sum) {
+			int low = 0;
+			int hi = data.Length - 1;
+			while (low < hi) {
+				int s = data[low] + data[hi];
+				if (s == sum) {
+					return true;
+				} else if (s < sum) {
+					low += 1;
+				} else {
+					hi -= 1;
+				}
+			}
+			return false;
+		}
 		static void Main(string[] args) {
-			int[] ints = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25 };
-			bool isFound = BinarySearch(ints, 10);
+			int[] ints = new int[] { 1, 2, 4, 4 };
+			bool isFound = HasPairWithSum(ints, 8);
 
 			Console.WriteLine(isFound);
 		}
